dd78c6bf57dd69874e2e0c276fbbedcc2add84c4
Danilo-Miguel <danilo.miguel@saojudas.br>
2023-10-09 20:51:31 -0300

    Criação da Result activity

--- app/src/main/java/com/example/androidotsintro/MainActivity.kt
+++ app/src/main/java/com/example/androidotsintro/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -14,9 +15,19 @@ class MainActivity : AppCompatActivity() {
 
         val btnEnviar = findViewById<Button>(R.id.btn_enviar)
         val txtResult  = findViewById<TextView>(R.id.txt_result)
+        val edText = findViewById<EditText>(R.id.edText)
+        val inputName = edText.text
 
         btnEnviar.setOnClickListener{
-            txtResult.text = "Danilo de Souza Miguel"
+           // txtResult.text = "Danilo de Souza Miguel"
+
+            if(inputName.isBlank()){
+                edText.error = "Esse campo não pode ser vazio"
+            }else{
+                txtResult.text = edText.text
+                btnEnviar.text = edText.text
+            }
+
         }
 
     }
---
+++ app/src/main/java/com/example/androidotsintro/Result.kt
@@ -0,0 +1,11 @@
+package com.example.androidotsintro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Result : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
